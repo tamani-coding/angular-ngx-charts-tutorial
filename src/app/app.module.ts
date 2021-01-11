49f88523a55761a2fbca7fc6beae6d3b47e9754d
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-11 20:23:34 +0100

    bubble chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PolarRadarChartComponent } from './polar-radar-chart/polar-radar-chart.
 import { LinearGaugeChartComponent } from './linear-gauge-chart/linear-gauge-chart.component';
 import { NumberCardChartComponent } from './number-card-chart/number-card-chart.component';
 import { TreeMapChartComponent } from './tree-map-chart/tree-map-chart.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { TreeMapChartComponent } from './tree-map-chart/tree-map-chart.component
     PolarRadarChartComponent,
     LinearGaugeChartComponent,
     NumberCardChartComponent,
-    TreeMapChartComponent
+    TreeMapChartComponent,
+    BubbleChartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bubble-chart/bubble-chart.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { bubbleData } from '../data/countries';
+
+@Component({
+  selector: 'app-bubble-chart',
+  templateUrl: './bubble-chart.component.html',
+  styleUrls: ['./bubble-chart.component.css']
+})
+export class BubbleChartComponent implements OnInit {
+
+  bubbleData: any[];
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = false;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  yAxisLabel: string = 'Population';
+  showYAxisLabel: boolean = true;
+  xAxisLabel: string = 'Years';
+  maxRadius: number = 20;
+  minRadius: number = 5;
+  yScaleMin: number = 70;
+  yScaleMax: number = 85;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { bubbleData });
+  }
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+  ngOnInit(): void {
+  }
+
+}
